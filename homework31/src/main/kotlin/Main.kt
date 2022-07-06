2d2df05567efc11a483627f17394c131c83fc50e
Starechok <108243692+Starechok@users.noreply.github.com>
2022-07-06 23:10:23 +1000

    Add files via upload

---
+++ homework31/src/main/kotlin/Main.kt
@@ -0,0 +1,55 @@
+const val DAY : Int = 24*60*60
+const val HOUR : Int = 60*60
+const val MIN : Int = 60
+
+fun main() {
+
+    println("был(а) " + agoToText(7*DAY))
+    println("был(а) " + agoToText(2*DAY + 5*HOUR))
+    println("был(а) " + agoToText(1*DAY + 3*HOUR))
+    println("был(а) " + agoToText(1*HOUR + 11*MIN))
+    println("был(а) " + agoToText(7*HOUR + 11*MIN))
+    println("был(а) " + agoToText(15*HOUR + 11*MIN))
+    println("был(а) " + agoToText(17*HOUR + 11*MIN))
+    println("был(а) " + agoToText(21*HOUR + 11*MIN))
+    println("был(а) " + agoToText(22*HOUR + 11*MIN))
+    println("был(а) " + agoToText(30*MIN))
+    println("был(а) " + agoToText(27*MIN))
+    println("был(а) " + agoToText(22*MIN))
+    println("был(а) " + agoToText(9*MIN))
+    println("был(а) " + agoToText(5*MIN))
+    println("был(а) " + agoToText(1*MIN + 2))
+    println("был(а) " + agoToText(24))
+}
+
+fun agoToText (countSec : Int) : String{
+    var text : String = ""
+    when {
+        countSec > 3*DAY ->  text = "давно"
+        countSec <= 3*DAY &&  countSec > 2*DAY ->  text = "вчера"
+        countSec <= 2*DAY &&  countSec > 1*DAY ->  text = "сегодня"
+        countSec in (HOUR + 1)..DAY ->
+        {
+            val hours : Int = (countSec/(HOUR)).toInt()
+            text = "$hours час" + endHours(hours) + " назад"
+        }
+        countSec in (MIN + 1)..HOUR ->  {
+            val minutes : Int = (countSec/(MIN)).toInt()
+            text = "$minutes минут" + endMinutes(minutes) + " назад"
+        }
+        else -> text = "только что"
+    }
+    return text
+}
+
+fun endHours(hours : Int) = when (hours % 10){
+    1 -> if (hours == 11) "ов" else ""
+    2,3,4 -> "а"
+    else -> "ов"
+}
+
+fun endMinutes(min : Int) = when (min % 10){
+    1 -> if (min == 11) "" else "у"
+    2,3,4 -> "ы"
+    else -> ""
+}
